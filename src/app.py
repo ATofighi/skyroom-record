af4700c94de305cca857875faf0083e3888b1ae3
AliReza <alirtofighim@gmail.com>
2020-09-18 17:48:11 +0430

    Fix flake8 issues.

--- src/app.py
+++ src/app.py
@@ -7,15 +7,18 @@ from flask import Flask, send_file, render_template, request
 app = Flask(__name__)
 pyautogui.FAILSAFE = False
 
+
 @app.route('/')
 def hello_world():
     return render_template('debug.html')
 
+
 @app.route('/screen')
 def screen():
     screenshot_path = '../downloads/my_screenshot.png'
-    im2 = pyautogui.screenshot(screenshot_path)
+    pyautogui.screenshot(screenshot_path)
     return send_file(screenshot_path, last_modified=datetime.now())
+
 
 @app.route('/click')
 def click_on_page():
@@ -24,6 +27,7 @@ def click_on_page():
     pyautogui.moveTo(x, y)
     pyautogui.click()
     return 'ok'
+
 
 @app.route('/get_pixel')
 def get_pixel():
